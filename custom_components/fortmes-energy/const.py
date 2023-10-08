40c7fe8885773fc38a9065b5d43b1cdb7a127465
Bakos, Attila <attila@bakosnet.hu>
2023-10-08 23:47:19 +0200

    Adjusted some files

---
+++ custom_components/fortmes-energy/const.py
@@ -0,0 +1,12 @@
+"""Constants for fortmes-energy."""
+from logging import Logger, getLogger
+
+LOGGER: Logger = getLogger(__package__)
+
+NAME = "Fortmes Energy"
+DOMAIN = "fortmes_energy"
+VERSION = "0.0.1"
+CLIENT_ID = "WxzBvwjGrWyjhcNs9vSH9dPqhE5mFplS"
+AUTH0_DOMAIN = "dev-fortmes.eu.auth0.com"
+
+ATTRIBUTION = "Data provided by http://api.fortmes.com/"
